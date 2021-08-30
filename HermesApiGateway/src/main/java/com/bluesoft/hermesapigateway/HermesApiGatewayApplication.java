d60bfb6ed82886caedc974d304de98120522a6ca
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-08-30 16:42:51 +0200

    Initialize Spring Cloud API Gateway

---
+++ HermesApiGateway/src/main/java/com/bluesoft/hermesapigateway/HermesApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.bluesoft.hermesapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HermesApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HermesApiGatewayApplication.class, args);
+    }
+
+}
--- HermesAppUsersService/src/main/java/com/bluesoft/ui/controllers/UserController.java
+++ HermesAppUsersService/src/main/java/com/bluesoft/ui/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.bluesoft.ui.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public  class UserController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/status/check")
     public String status(){
-        return "Working...";
+        return "Working on port " + environment.getProperty("local.server.port");
     }
 
 }
