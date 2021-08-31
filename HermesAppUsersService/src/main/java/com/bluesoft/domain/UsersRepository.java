62574436996ffec53aaf878cce1af71fcdb154b0
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-08-31 18:04:43 +0200

    adding implementation of method createUser from UsersApplicationService class to store user in database

--- HermesAppUsersService/src/main/java/com/bluesoft/domain/UsersRepository.java
+++ HermesAppUsersService/src/main/java/com/bluesoft/domain/UsersRepository.java
@@ -1,4 +1,6 @@
 package com.bluesoft.domain;
 
 public interface UsersRepository {
+
+    void save(UserEntity userEntity);
 }
--- HermesAppUsersService/src/main/java/com/bluesoft/infrastructures/persistence/jpa/users/JpaUsersRepository.java
+++ HermesAppUsersService/src/main/java/com/bluesoft/infrastructures/persistence/jpa/users/JpaUsersRepository.java
@@ -1,5 +1,6 @@
 package com.bluesoft.infrastructures.persistence.jpa.users;
 
+import com.bluesoft.domain.UserEntity;
 import com.bluesoft.domain.UsersRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,10 @@ class JpaUsersRepository implements UsersRepository {
 
     JpaUsersRepository(final SpringJpaUsersRepository springJpaUsersRepository) {
         this.springJpaUsersRepository = springJpaUsersRepository;
+    }
+
+    @Override
+    public void save(final UserEntity userEntity) {
+        springJpaUsersRepository.save(userEntity);
     }
 }
