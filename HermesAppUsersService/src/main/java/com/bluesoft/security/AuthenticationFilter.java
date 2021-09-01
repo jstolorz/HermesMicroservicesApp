d79b6b4ddddabf6917a2cc233fdbea98f2036e3e
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-09-01 16:51:14 +0200

    login - adding AuthenticationFilter to create login functionality

---
+++ HermesAppUsersService/src/main/java/com/bluesoft/security/AuthenticationFilter.java
@@ -0,0 +1,36 @@
+package com.bluesoft.security;
+
+
+import com.bluesoft.ui.model.LoginRequestModel;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    @Override
+    public Authentication attemptAuthentication(final HttpServletRequest request, final HttpServletResponse response) throws AuthenticationException {
+       try{
+
+           LoginRequestModel creds = new ObjectMapper()
+                   .readValue(request.getInputStream(), LoginRequestModel.class);
+
+           return getAuthenticationManager().authenticate(
+                   new UsernamePasswordAuthenticationToken(
+                           creds.getEmail(),
+                           creds.getPassword(),
+                           new ArrayList<>()
+                   )
+           );
+
+       }catch (IOException e){
+           throw new RuntimeException(e);
+       }
+    }
+}
